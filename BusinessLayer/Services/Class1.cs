24f3a887a517e40c7127be1713f1f6c546cf6122
itdev-lenovo <it1devops@gmail.com>
2020-03-27 17:23:06 +0530

    files added

---
+++ BusinessLayer/Services/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer.Services
+{
+    class Class1
+    {
+        //Write a query to filter and sort the data in the table using where and order by claus
+
+
+
+
+    }
+}
